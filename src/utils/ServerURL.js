d362089e01048d08742cc57f0a1c7e9acf89efdb
GreenPlanet0204 <midasshong@gmail.com>
2023-08-11 01:40:16 -0800

    fix

--- src/utils/ServerURL.js
+++ src/utils/ServerURL.js
@@ -1,6 +1,6 @@
 const ServerURL = {
   BASE_URL: "https://lobster-app-mhbu4.ondigitalocean.app",
-  WS_BASE_URL: "ws://lobster-app-mhbu4.ondigitalocean.app",
+  WS_BASE_URL: "wss://lobster-app-mhbu4.ondigitalocean.app",
 };
 
 export default ServerURL;
